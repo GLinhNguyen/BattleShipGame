d56c45834688d83473132eca526bec66bb231d5a
jonathandanghokcode <144365683+jonathandanghokcode@users.noreply.github.com>
2024-05-11 13:07:42 +0700

    update easybot,marker and change the value in coordinate from private to protected

---
+++ src/EasyBot.java
@@ -0,0 +1,28 @@
+public class EasyBot extends Bot{
+    // randoming the order to move
+    // playegrid reference the player 's grid to attack
+    public EasyBot(SelectionGrid playerGrid) {
+        super(playerGrid);
+        Collections.shuffle(validMoves);
+    }
+
+
+     //Resets the AI by resetting the parent class, and then
+     //reshuffling the refreshed list of valid moves.
+
+    @Override
+    public void reset() {
+        super.reset();
+        Collections.shuffle(validMoves);
+    }
+
+    //Takes the move from the top of the list and returns it and return A position from the valid moves list.
+    
+    @Override
+    public Location selectMove() {
+        Location nextMove = validMoves.get(0);
+        validMoves.remove(0);
+        return nextMove;
+    }
+}
+
